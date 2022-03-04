1b75be133aa5f0c077095b0900b71f06c1db33d3
李迎飞 <yingfei.li@chetuobang.com>
2022-03-04 16:05:05 +0800

    1. 增加正则匹配工具类

---
+++ src/test/java/org/lyf/utillity/example/RegexTest.java
@@ -0,0 +1,22 @@
+package org.lyf.utillity.example;
+
+import org.lyf.utillity.regex.RegexUtil;
+
+/**
+ * @author L.Y.F
+ * @date 2022/3/4 3:42 下午
+ */
+public class RegexTest {
+
+    public static void main(String[] args) {
+        String text = "afnjdka@163.com";
+        String text1 = "afnjdka@163";
+        // 测试文本是否符合邮件规则
+        System.out.println(RegexUtil.isMatching(text, RegexUtil.EMAIL_REGEX));
+        // 测试文本是否符合邮件规则
+        System.out.println(RegexUtil.isMatching(text1, RegexUtil.EMAIL_REGEX));
+        // 查询指定文本是否包含指定的字符
+        System.out.println(RegexUtil.isContain(text1, "[@]"));
+    }
+
+}
