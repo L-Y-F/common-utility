e2b900a5f9dfdc7637b6c0b6663c01797a2517a4
李迎飞 <nb114917778@163.com>
2023-11-23 15:57:30 +0800

    1. 切换工具类

--- src/main/java/org/sljl/utillity/utils/mobile/MobileUtil.java
+++ src/main/java/org/sljl/utillity/utils/mobile/MobileUtil.java
@@ -1,7 +1,7 @@
 package org.sljl.utillity.utils.mobile;
 
 
-import org.apache.commons.lang3.StringUtils;
+import org.sljl.utillity.utils.basic.StrUtil;
 
 import java.util.regex.Pattern;
 
@@ -13,34 +13,34 @@ import java.util.regex.Pattern;
  *
  */
 public class MobileUtil {
-
+	
 	/**
 	 * 中国电信号码格式验证 手机段： 133,149,153,173,177,180,181,189,193,199,1349,1410,1700,1701,1702
-	 *
+	 * 
 	 **/
 	private static final String CHINA_TELECOM_PATTERN = "(?:^(?:\\+86)?1(?:33|49|53|7[37]|8[019]|9[39])\\d{8}$)|(?:^(?:\\+86)?1349\\d{7}$)|(?:^(?:\\+86)?1410\\d{7}$)|(?:^(?:\\+86)?170[0-2]\\d{7}$)";
-
+	
 	/**
 	 * 中国联通号码格式验证 手机段：130,131,132,145,146,155,156,166,171,175,176,185,186,1704,1707,1708,1709
-	 *
+	 * 
 	 **/
 	private static final String CHINA_UNICOM_PATTERN = "(?:^(?:\\+86)?1(?:3[0-2]|4[56]|5[56]|6[67]|7[156]|8[56])\\d{8}$)|(?:^(?:\\+86)?170[47-9]\\d{7}$)";
-
+	
 	/**
 	 * 中国移动号码格式验证
 	 * 手机段：134,135,136,137,138,139,147,148,150,151,152,157,158,159,178,182,183,184,187,188,191,192,194,195,196,197,198,1440,1703,1705,1706
-	 *
+	 * 
 	 **/
 	private static final String CHINA_MOBILE_PATTERN = "(?:^(?:\\+86)?1(?:3[4-9]|4[78]|5[0-27-9]|7[28]|8[2-478]|9[124-8])\\d{8}$)|(?:^(?:\\+86)?1440\\d{7}$)|(?:^(?:\\+86)?170[356]\\d{7}$)";
 
 	/**
 	 * 中国大陆手机号码校验
-	 *
+	 * 
 	 * @param phone
 	 * @return
 	 */
 	public static boolean checkPhone(String phone) {
-		if (StringUtils.isNotBlank(phone)) {
+		if (StrUtil.isNotBlank(phone)) {
 			if (checkChinaMobile(phone) || checkChinaUnicom(phone) || checkChinaTelecom(phone)) {
 				return true;
 			}
@@ -50,12 +50,12 @@ public class MobileUtil {
 
 	/**
 	 * 中国移动手机号码校验
-	 *
+	 * 
 	 * @param phone
 	 * @return
 	 */
 	public static boolean checkChinaMobile(String phone) {
-		if (StringUtils.isNotBlank(phone)) {
+		if (StrUtil.isNotBlank(phone)) {
 			Pattern regexp = Pattern.compile(CHINA_MOBILE_PATTERN);
 			if (regexp.matcher(phone).matches()) {
 				return true;
@@ -66,12 +66,12 @@ public class MobileUtil {
 
 	/**
 	 * 中国联通手机号码校验
-	 *
+	 * 
 	 * @param phone
 	 * @return
 	 */
 	public static boolean checkChinaUnicom(String phone) {
-		if (StringUtils.isNotBlank(phone)) {
+		if (StrUtil.isNotBlank(phone)) {
 			Pattern regexp = Pattern.compile(CHINA_UNICOM_PATTERN);
 			if (regexp.matcher(phone).matches()) {
 				return true;
@@ -82,12 +82,12 @@ public class MobileUtil {
 
 	/**
 	 * 中国电信手机号码校验
-	 *
+	 * 
 	 * @param phone
 	 * @return
 	 */
 	public static boolean checkChinaTelecom(String phone) {
-		if (StringUtils.isNotBlank(phone)) {
+		if (StrUtil.isNotBlank(phone)) {
 			Pattern regexp = Pattern.compile(CHINA_TELECOM_PATTERN);
 			if (regexp.matcher(phone).matches()) {
 				return true;
@@ -103,22 +103,10 @@ public class MobileUtil {
 	 * @return java.lang.String
 	 */
 	public static String hideFourMiddleNumber(String phone) {
-		if (StringUtils.isBlank(phone)) {
+		if (StrUtil.isBlank(phone)) {
 			return phone;
 		}
 		return phone.replaceAll("(\\d{3})\\d{4}(\\d{4})","$1****$2");
-	}
-
-	public static void main(String[] args) {
-		System.out.println(checkPhone("16721690017"));
-		//		System.out.println(checkPhone("19231855100"));
-		//		System.out.println(checkPhone("19331855100"));
-		//		System.out.println(checkPhone("19431855100"));
-		//		System.out.println(checkPhone("19531855100"));
-		//		System.out.println(checkPhone("19631855100"));
-		//		System.out.println(checkPhone("19731855100"));
-		//		System.out.println(checkPhone("19831855100"));
-		//		System.out.println(checkPhone("19931855100"));
 	}
 	
 }
